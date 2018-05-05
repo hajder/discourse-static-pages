aef683ffffc8d3e8f99e9a20353076fcebf0f0a8
Mike Szyndel <msz@sent.com>
2018-05-05 21:21:40 +0200

    add migration versions

--- db/migrate/20160922130233_add_stylesheet_to_static_pages_pages.rb
+++ db/migrate/20160922130233_add_stylesheet_to_static_pages_pages.rb
@@ -1,4 +1,4 @@
-class AddStylesheetToStaticPagesPages < ActiveRecord::Migration
+class AddStylesheetToStaticPagesPages < ActiveRecord::Migration[5.1]
   def change
     add_column :static_pages_pages, :stylesheet, :text
     add_column :static_pages_pages, :stylesheet_baked, :text
--- db/migrate/20161216200136_add_path_to_static_pages_pages.rb
+++ db/migrate/20161216200136_add_path_to_static_pages_pages.rb
@@ -1,4 +1,4 @@
-class AddPathToStaticPagesPages < ActiveRecord::Migration
+class AddPathToStaticPagesPages < ActiveRecord::Migration[5.1]
   def change
     add_column :static_pages_pages, :path, :string
     add_index :static_pages_pages, :path, unique: true
